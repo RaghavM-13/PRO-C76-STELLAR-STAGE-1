14be90e0f9629b4d908948f4dfd728571d129744
RaghavM-13 <73744864+RaghavM-13@users.noreply.github.com>
2021-08-09 13:55:00 +0100

    Add files via upload

---
+++ Project-76-main/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import StackUtils from 'stack-utils';
+import DailyPicScreen from './screens/DailyPic';
+import SpaceCraftsScreen from './screens/SpaceCrafts';
+import StarMapScreen from './screens/StarMap';
+
+export default function App() {
+  return (
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Home" screenOptions = {{
+        headerShown : false
+      }}>
+      <Stack.Screen name = "Home" component = {{HomeScreen}}/>
+      <Stack.Screen name = "SpaceCrafts" component = {{SpaceCraftsScreen}}/>
+      <Stack.Screen name = "DailyPic" component = {{DailyPicScreen}}/>
+      <Stack.Screen name = "StarMap" component = {{StarMapScreen}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
